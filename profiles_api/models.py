ff3501ed164687fac0fa2971b1261916e37c029a
Goku2699 <vineetkumar2611@gmail.com>
2023-07-12 12:29:45 +0530

    Final changes

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,6 +1,7 @@
 import email
 from unicodedata import name
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
@@ -46,9 +47,22 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """Retreive full name of user"""
         return self.name
 
-    def get_full_name(self):
-        """Retreive full name of user"""
+    def get_short_name(self):
+        """Retrieve shot name of user"""
         return self.name
 
     def __str__(self):
         return self.email
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
